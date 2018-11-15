02c24d0c7f4c49be5beab060c8db1a027a1112e0
Christoffer Petersen <fantomlich.fujie@gmail.com>
2018-11-15 13:42:28 +0100

    Her er testen håber det gik rigtigt i kraken

--- OOPH1Eksamen/OOPH1Eksamen/Program.cs
+++ OOPH1Eksamen/OOPH1Eksamen/Program.cs
@@ -16,6 +16,8 @@ namespace OOPH1Eksamen
         public static void Main() {
             //Denne funktion kan du bruge til at teste din kode
             Console.WriteLine("Hello World!");
+            D d = new D();
+
         }
     }
 
@@ -24,24 +26,34 @@ namespace OOPH1Eksamen
     {
         public string name = "";
         public int legs = 2;
+        public int age;
     }
 
     //OPG Tilføj en string property kaldet 'brand' til følgende class
     class E
     {
-
+        string brand;
     }
 
     //OPG Tilføj en metode der kan forstørre 'a' med 1 hver gang den kaldes
     class C
     {
         public int a;
+        public int AEnlargement()
+        {
+            a += 1;
+            return a;
+        }
     }
 
     //OPG Tilføj en metode kaldet "getA" der kan returnere værdien i a
     class G
     {
         string a = "Hello!";
+        public string getA()
+        {
+            return a;
+        }
     }
 
     //OPG Tilføj en metode der kan tilføje et tal til numbers. Numbers skal
@@ -49,14 +61,19 @@ namespace OOPH1Eksamen
     class F
     {
         List<int> numbers = new List<int>();
+        string input = Console.ReadLine();
+        public void AddNumberToNumbers()
+        {
+            numbers.Add(Int32.Parse(input));
+        }
     }
 
     //OPG Implementer encapsulation i denne klasse, men gør således det stadig 
     //er muligt at læse værdierne 'a' og 'b'
     class D
-    {
+    {   //Her mangler jeg virkelig bedre forståelse for har ikke nogen ide om hvordan det skal gøres bliver forvirret over alle de ting jeg læser på nettet.
         public int a;
-        public string b;
+        public string b;        
     }
 
     //OPG Lav en constructor der sætter alle properties af følgende klasse
@@ -65,29 +82,42 @@ namespace OOPH1Eksamen
         public int a;
         public string b;
         public double c;
+
+        public A(int a = 0, string b = "default", double c = 0.0)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
     }
 
     //OPG klassen H skal have en reference til klasse A, kaldet 'a'. Lav en constructor der
     //sætter denne til reference til et objekt af A
     class H
     {
-        
+        A a = new A();
+
+        public H(A a)
+        {
+            this.a = a;
+        }
     }
 
     //OPG Ændre funktionen PrintM, således at den ikke crasher programmet hvis den køres.
     class I 
-    {
+    {   //Denne opgave drev mig ud på et sidespor til jeg opdagede hvor retarderet min hjerne er.
         public string text;
 
         public void PrintM(){
-            Console.WriteLine("Text is " + text.Length + "long");
+            text = "default";
+            Console.WriteLine("Text is " + text.Length + " long");
         }
     }
 
     //OPG Invoke kan faile hvis der gives en null værdi til dens constructor.
     //sørg for at dette ikke kan forekomme ved kun at ændre på Invoke
     class J
-    {
+    {   //Her mangler jeg virkelig bedre forståelse for har ikke nogen ide om hvordan det skal gøres bliver forvirret over alle de ting jeg læser på nettet.
         public I i;
 
         public void Invoke() {
@@ -102,8 +132,8 @@ namespace OOPH1Eksamen
     //OPG Herunder skal du selv implementere en class kaldet MyClass der
     //er subclass til C. Tilføj et public string felt kaldet 'myString'.
 
-    class MyClass {
-        
+    class MyClass : C {
+        public string myString;
     }
 
 
